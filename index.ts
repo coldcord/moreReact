c3e2b3de5d3b745fa42107dc3b8c7e2fa720ec2c
programminglaboratorys <107296738+programminglaboratorys@users.noreply.github.com>
2024-11-13 01:03:16 +0300

    fix patch
    not tested

--- index.ts
+++ index.ts
@@ -39,7 +39,7 @@ export default definePlugin({
     ],
     patches: [
         {
-            find: ".MESSAGE_HOVER_BAR_REACTION_SUGGESTION_ADD_ALLY",
+            find: "location:\"useMessageUtilitiesProps\"",
             replacement: {
                 match: /(\.length>=3?.{0,40}\.slice\(0),3\)/,
                 replace: "$1,$self.settings.store.Max)"
